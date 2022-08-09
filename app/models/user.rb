2d6172127d2f8d60816bf952df5d883b681c78f4
Steven Ngov <gl.steven.ngov@gmail.com>
2022-08-09 13:17:01 -0400

    Define new class method authenticate_with_credentials on the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ApplicationRecord
 
   validates :password, length: { minimum: 4 }
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
 end
