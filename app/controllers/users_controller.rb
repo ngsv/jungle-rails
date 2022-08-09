12e5a202f0bd21df3a94b79ccc6f2e94a90c205d
Steven Ngov <gl.steven.ngov@gmail.com>
2022-08-09 12:09:52 -0400

    Fix error messages during user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/', notice: 'User created!'
     else
       render :new
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
 
-  has_secure_password
-
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
+
+  has_secure_password
 
 end
