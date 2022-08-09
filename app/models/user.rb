65836a71839f9146ad21092ece9cce0d9462cb5e
Steven Ngov <gl.steven.ngov@gmail.com>
2022-08-09 16:24:38 -0400

    Model spec: User

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,15 @@ class User < ApplicationRecord
 
   validates :password, length: { minimum: 4 }
 
+  # Converts email to lowercase before saving to database
+  before_save :downcase_fields
+
+  def downcase_fields
+    self.email.downcase!
+  end
+
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email)
+    user = User.find_by_email(email.strip.downcase)
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(password)
       return user
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-
     it 'is invalid without a password field' do
       @user = User.new(:first_name => "Steven", :last_name => "Ngov", :email => "gl.steven.ngov@gmail.com", :password => nil, :password_confirmation => "123")
       expect(@user).to be_invalid
@@ -38,6 +37,43 @@ RSpec.describe User, type: :model do
       @existing_user.save
       @user = User.new(:first_name => "Steven", :last_name => "Ngov", :email => "TEST@TEST.com", :password => "123", :password_confirmation => "123")
       expect(@user).to be_invalid
+    end
+  end
+
+
+  describe '.authenticate_with_credentials' do
+
+    it 'successfully authenticates with valid credentials' do
+      @user = User.new(:first_name => "Steven", :last_name => "Ngov", :email => "gl.steven.ngov@gmail.com", :password => "12345", :password_confirmation => "12345")
+      @user.save
+      @auth = User.authenticate_with_credentials("gl.steven.ngov@gmail.com", "12345")
+      expect(@auth).to eq(@user)
+    end
+
+    it 'does not authenticate if email is not found in the database' do
+      @auth = User.authenticate_with_credentials("gl.steven.ngov@gmail.com", "12345")
+      expect(@auth).to be_nil
+    end
+
+    it 'does not authenticate if password is incorrect' do
+      @user = User.new(:first_name => "Steven", :last_name => "Ngov", :email => "gl.steven.ngov@gmail.com", :password => "12345", :password_confirmation => "12345")
+      @user.save
+      @auth = User.authenticate_with_credentials("gl.steven.ngov@gmail.com", "abcde")
+      expect(@auth).to be_nil
+    end
+
+    it 'successfully authenticates with leading and trailing spaces in the email field' do
+      @user = User.new(:first_name => "Steven", :last_name => "Ngov", :email => "example@domain.com", :password => "12345", :password_confirmation => "12345")
+      @user.save
+      @auth = User.authenticate_with_credentials(" example@domain.com ", "12345")
+      expect(@auth).to eq(@user)
+    end
+
+    it 'successfully authenticates for both uppercase and lowercase characters in the email field' do
+      @user = User.new(:first_name => "Steven", :last_name => "Ngov", :email => "eXample@domain.COM", :password => "12345", :password_confirmation => "12345")
+      @user.save
+      @auth = User.authenticate_with_credentials("EXAMPLe@DOMAIN.CoM", "12345")
+      expect(@auth).to eq(@user)
     end
 
   end
